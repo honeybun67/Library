085fe924af1df5392b0895b10c208ceef1f4d4b6
honeybun67 <havsmollova@icloud.com>
2025-02-04 20:04:08 +0200

    creating new models and new tables

---
+++ Library/Data/Models/Author.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Library.Data.Models
+{
+    public class Author
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        [MaxLength (64)]
+        public string Name { get; set; }
+
+        [MaxLength(255)]
+        public string Description { get; set; }
+
+        public string Image { get; set; }
+
+    }
+}
